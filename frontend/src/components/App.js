7e0b3908e9fb3806f0cffa8894e6d24db0722acf
EmilePW <emilepw@hotmail.co.uk>
2016-09-20 20:36:43 +0100

    Use reeact router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
         <Navigation />
         <LandingPage />
         <Footer />
-        <Uploader />
       </div>
     )
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 import iapApp from './reducers'
 import App from './components/App'
@@ -14,15 +14,11 @@ let store = createStore(iapApp)
 
 const rootEl = document.getElementById('root')
 
-
-// const routes = <Router history={hashHistory}>
-//                   <Route path="/" component={App} />
-//                   <Route path="/upload" component={Uploader} />
-//                 </Router>
-
 render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>,
   rootEl
 )
