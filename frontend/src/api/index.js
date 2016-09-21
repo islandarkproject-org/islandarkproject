c1168d15da5e4b488f3d917473650616a1dc5c92
EmilePW <emilepw@hotmail.co.uk>
2016-09-21 22:33:54 +0100

    Beginning of api for fetching data (and calling python services?)

---
+++ frontend/src/api/index.js
@@ -0,0 +1,17 @@
+export function getJSON (url) {
+  let d = new Promise((resolve reject) => {
+    let req = new XMLHttpRequest()
+    req.open('GET', url)
+    req.send()
+
+    req.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(this.response)
+      } else {
+        reject(this.statusText)
+      }
+    }
+  })
+  .then((responseData) => JSON.parse(responseData))
+  .catch((error) => console.log(error))
+}
