1058686f0dd6f7ec9453bf29cc94b4007c9b5bb0
EmilePW <emilepw@hotmail.co.uk>
2016-09-22 17:39:34 +0100

    Import store instead of setting up in entry point for separation of concerns

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,13 @@ require('./styles/main.scss')
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import iapApp from './reducers'
 import App from './components/App'
 import LandingPage from './components/LandingPage'
 import TeamPage from './components/TeamPage'
 
-let store = createStore(iapApp)
+import store from './configureStore'
 
 const rootEl = document.getElementById('root')
 
