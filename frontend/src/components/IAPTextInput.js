a8f5ff2fe72e0e49a8ded99a2dfd9b0b8193a33e
EmilePW <emilepw@hotmail.co.uk>
2016-09-26 15:48:13 +0100

    Upload form layout + styling improvements

--- frontend/src/components/IAPTextInput.js
+++ frontend/src/components/IAPTextInput.js
@@ -3,13 +3,12 @@ import React, { PropTypes } from 'react'
 class IAPTextInput extends React.Component {
   render () {
     return (
-      <input className='IAPTextInput' type={this.props.type} placeholder={this.props.placeholder} value={this.props.value} />
+      <input className='IAPTextInput' type='text' placeholder={this.props.placeholder} value={this.props.value} />
     )
   }
 }
 
 IAPTextInput.propTypes = {
-  type: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string
 }
--- frontend/src/components/UploadForm.js
+++ frontend/src/components/UploadForm.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import IAPTextInput from './IAPTextInput'
+import IAPCheckBox from './IAPCheckBox'
 
 class UploadForm extends React.Component {
   render () {
     return (
       <form className='UploadForm'>
-        <IAPTextInput type='text' placeholder='Enter a name' value={this.props.name} />
-        <textarea placeholder='Describe this upload'>{this.props.description}</textarea>
-        <IAPTextInput type='text' placeholder='Celebration, language, etc.' value={this.props.tags.join(', ')} />
-        <input type='checkbox' checked={this.props.isPrivate} />
-        <IAPTextInput type='text' placeholder='Enter the location' value={this.props.location} />
+        <label>
+          <h4>Name Your Upload</h4>
+          <IAPTextInput placeholder='Enter a name' value={this.props.name} />
+        </label>
+        <label>
+          <h4>Describe Your Upload</h4>
+          <textarea placeholder='Describe this upload'>{this.props.description}</textarea>
+        </label>
+        <label>
+          <h4>Tag Your Upload</h4>
+          <IAPTextInput placeholder='Celebration, language, etc.' value={this.props.tags.join(', ')} />
+        </label>
+        <label>
+          <h4>Make this public? Remove the check to make it private.</h4>
+          <IAPCheckBox checked={!this.props.isPrivate} />
+        </label>
+        <label>
+          <h4>Location of the Upload</h4>
+          <IAPTextInput placeholder='Enter the location' value={this.props.location} />
+        </label>
       </form>
     )
   }
+}
+
+UploadForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.string),
+  isPrivate: PropTypes.bool.isRequired,
+  location: PropTypes.string
 }
 
 export default UploadForm
