85b57b5b3b90038697fbe74fe1fa54ed4b30abab
EmilePW <emilepw@hotmail.co.uk>
2016-10-01 18:58:28 +0100

    Intialise testing setup

---
+++ frontend/tests/empty.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect'
+
+describe('Empty Test', () => {
+  it('Should always work', () => {
+    const actual = true
+    const expected = true
+
+    expect(actual).toEqual(expected)
+  })
+})
