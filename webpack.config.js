dc5c48d3da343d8b7a02bb2de25def121bf14dab
EmilePW <emilepw@hotmail.co.uk>
2016-10-01 22:57:01 +0100

    Reconfigured webpack with v2 and helpful scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
-module.exports = {
-  entry: './frontend/src/index.js',
-  output: {
-    filename: './static/bundle.js'
-  },
-  devServer: {
-    port: 3000,
-    inline: true
-  },
-  debug: true,
-  devtool: '#eval-source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
+const {resolve} = require('path')
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: resolve(__dirname, 'static'),
+      pathinfo: !env.prod
+    },
+    context: resolve(__dirname, 'frontend'),
+    devtool: env.prod ? 'source-map' : 'eval',
+    bail: env.prod,
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.s?css$/,
+          loaders: ['style', 'css', 'sass']
         }
-      },
-      {
-        test: /\.s?css$/,
-        loaders: ['style', 'css', 'sass'],
-        inlcude: './frontend/src'
-      }
-    ]
+      ]
+    }
   }
 }
