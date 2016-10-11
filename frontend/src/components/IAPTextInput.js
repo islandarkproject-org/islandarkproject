33cac86a58d4bd046c5873863e47cb48e939641c
EmilePW <emilepw@hotmail.co.uk>
2016-10-11 18:50:30 +0100

    Rearranged layout of props on text input component and added support for onFocus

--- frontend/src/components/IAPTextInput.js
+++ frontend/src/components/IAPTextInput.js
@@ -3,7 +3,14 @@ import React, { PropTypes } from 'react'
 class IAPTextInput extends React.Component {
   render () {
     return (
-      <input className='IAPTextInput' type='text' placeholder={this.props.placeholder} defaultValue={this.props.value} onChange={this.props.onChange} />
+      <input
+        className='IAPTextInput'
+        type='text'
+        placeholder={this.props.placeholder}
+        defaultValue={this.props.value}
+        onChange={this.props.onChange}
+        onFocus={this.props.onFocus}
+      />
     )
   }
 }
