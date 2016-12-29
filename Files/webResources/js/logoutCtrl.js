0e037239ee7c4c3f532790b7496d1b2eb022aec5
EmilePW <emilepw@hotmail.co.uk>
2016-12-29 23:02:07 +0000

    Removed old code (Angular, CSS, etc.)

--- Files/webResources/js/logoutCtrl.js
+++
@@ -1,4 +0,0 @@
-	app.controller('LogoutController',function($scope)
-	{
-		$scope.credentials = {url:'/login', option:"Login"};
-	});
--- Files/webResources/uploads/getUploadedFiles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$arr = scandir( 'patron_uploads' );
-		
-	//	$arrToSend = array("status"=>"error", "errorMessage"=>"dirToSearch: $dirToSearch");		
-		$arrToSend = array('fileNames'=>$arr, "status"=>"successful");
-		echo json_encode($arrToSend);
-
-?>
